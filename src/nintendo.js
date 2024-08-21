f3c0977f2853a6aa7a363475cb6b5a90abcb9052
김민주 <lenil1@naver.com>
2024-08-21 21:17:15 +0900

    0821

--- src/nintendo.js
+++ src/nintendo.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import YouTube from 'react-youtube';
 
 const Nintendo = () => {
     // 드롭다운
@@ -17,6 +18,7 @@ const Nintendo = () => {
     //[ Sec 01 ]
         const slideWidth = 744;
         const [currentPosition, setCurrentPosition] = useState(0);
+        const[isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 1200);
     
         const prevSlide = () => {
             let newPosition = currentPosition + slideWidth;
@@ -32,7 +34,14 @@ const Nintendo = () => {
                 newPosition = 0;
             }
             setCurrentPosition(newPosition);
-        }
+        };
+        useEffect(() => {
+            const handleResize = () => {
+                setIsSmallScreen(window.innerWidth <= 1200);
+            };
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
 
     //[ Sec 03 ]
         const containersRef = useRef([]);
@@ -413,6 +422,19 @@ const Nintendo = () => {
                     backgroundPosition: `${currentPosition}px 0px`, 
                     transition: 'background-position 0.7s' }}></div>
             </div>
+            {isSmallScreen && (
+                    <div className="video-container" style={{ marginTop: '50px', textAlign: 'center' }}>
+                        <iframe
+                            width="100%"
+                            height="315"
+                            src="https://youtu.be/JEPvt1_DjmE?si=M5ZIy0IJjMUYgYLx"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                )}
         </div>
         <img className="bg-orange" src="../public/image/Sec01/orange.png"/>
     </section>
