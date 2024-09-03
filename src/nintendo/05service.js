f3fa9d66b4a7c36832928f4d25d7ee7f61b2f7fe
김민주 <lenil1@naver.com>
2024-09-04 04:28:00 +0900

    0904

--- src/nintendo/05service.js
+++ src/nintendo/05service.js
@@ -6,25 +6,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Service = () => {
 
-  useEffect(() => {
-    
-    gsap.from(".service-list li img", {
-      y: 200, 
-      opacity: 0, 
-      duration: 1,
-      ease: "power2.out",
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: "#service",  
-        start: "40% 28%",     
-        end: "bottom 28%",         
-        markers: true,         
-        pin: true,             
-        scrub: true,           
-      },
-    });
-  }, []);
-
   return (
     <>
       <section className="service" style={{ width: "100%" }}>
